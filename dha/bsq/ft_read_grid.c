4a534f3c87c8e160eeb98cafdecd9db77d1e90d4
AlphaAOmega <france998811@gmail.com>
2021-10-24 13:43:00 +0900

    1

--- dha/bsq/ft_read_grid.c
+++ dha/bsq/ft_read_grid.c
@@ -1,38 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_read_first_row.c                                :+:      :+:    :+:   */
+/*   ft_read_grid.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/24 10:56:38 by dha               #+#    #+#             */
-/*   Updated: 2021/10/24 13:36:36 by dha              ###   ########.fr       */
+/*   Created: 2021/10/24 13:22:34 by dha               #+#    #+#             */
+/*   Updated: 2021/10/24 13:42:20 by dha              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-int	ft_read_first_row(int fd, t_map *map)
+int	ft_read_grid(int fd, t_map *map)
 {
 	int		i;
+	int		cnt;
 	char	c;
 
-	i = 0;
-	map->grid[0] = (char *) malloc(sizeof(char) * 16);
-	map->col = 16;
-	while (1)
+	i = 1;
+	while (i < map->row)
 	{
-		read(fd, &c, 1);
-		if (c == '\n')
-			break ;
-		if (c != map->empty && c != map->obstacle) // C가 NULL 또는 non printable 이면?
-			return (0);
-		grid[0][i] = c;
-		i++;
-		if (i == map->col)
+		map->grid[i] = (char *) malloc(sizeof(char) * map->col);
+		cnt = 0;
+		while (cnt < map->col)
 		{
-			// realloc
+			read(fd, &c, 1);
+			if (c == '\n')
+				return (0);
+			if (c != map->empty && c != map->obstacle)
+				return (0);
+			grid[i][cnt] = c;
+			cnt++;
 		}
+		read(fd, &c, 1);
+		if (c != '\n')
+			return (0);
+		i++;
 	}
-	return (i);
 }
--- dha/bsq/input.c
+++ dha/bsq/input.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 23:06:13 by dha               #+#    #+#             */
-/*   Updated: 2021/10/24 10:54:55 by dha              ###   ########.fr       */
+/*   Updated: 2021/10/24 13:35:13 by dha              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	input(char *filename, t_map *map)
 {
 	int		fd;
-	t_map	*map;
 
 	if (filename == 0)
 		fd = 0;
@@ -23,10 +22,10 @@ int	input(char *filename, t_map *map)
 		fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	map = (t_map *) malloc(sizeof(t_map));
 	close(fd);
 	if (!ft_read_first_line(fd, map))
 		return (0);
+	map->grid = (char **) malloc(sizeof(char *) * map->row);
 	if (!ft_read_first_row(fd, map))
 		return (0);
 	if (!ft_read_grid(fd, map))
