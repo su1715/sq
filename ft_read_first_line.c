bd50e308daafaf8ab41df13bc45f10acb209d7dd
AlphaAOmega <france998811@gmail.com>
2021-10-27 01:40:50 +0900

    fix read_first_line

--- ft_read_first_line.c
+++ ft_read_first_line.c
@@ -6,11 +6,18 @@
 /*   By: dha <dha@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 10:40:35 by dha               #+#    #+#             */
-/*   Updated: 2021/10/26 16:15:38 by dha              ###   ########.fr       */
+/*   Updated: 2021/10/27 01:40:04 by dha              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq_lib.h"
+
+void	get_map_char(t_map *map, char *buf, int i)
+{
+	map->empty = buf[i - 3];
+	map->obstacle = buf[i - 2];
+	map->fill = buf[i - 1];
+}
 
 int	ft_read_first_line(int fd, t_map *map)
 {
@@ -20,14 +27,13 @@ int	ft_read_first_line(int fd, t_map *map)
 	i = 0;
 	while (1)
 	{
-		if (i > 13)
+		if (i > 13 || read(fd, buf + i, 1) < 1)
 			return (0);
-		read(fd, buf + i, 1);
 		if (buf[i] == '\n')
 		{
-			map->empty = buf[i - 3];
-			map->obstacle = buf[i - 2];
-			map->fill = buf[i - 1];
+			if (i < 4)
+				return (0);
+			get_map_char(map, buf, i);
 			if (is_overlap(map->empty, map->obstacle, map->fill))
 				return (0);
 			buf[i - 3] = '\0';
